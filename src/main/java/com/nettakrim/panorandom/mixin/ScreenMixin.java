f856755d7b2f341174930d0c42ecb636ac6e5fef
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2025-12-24 14:09:15 +0000

    what??? why does it run fine when the mixins are invalid?????

--- src/main/java/com/nettakrim/panorandom/mixin/ScreenMixin.java
+++ src/main/java/com/nettakrim/panorandom/mixin/ScreenMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Screen.class)
 public class ScreenMixin {
-    @Inject(method = "init(Lnet/minecraft/client/MinecraftClient;II)V", at = @At("TAIL"))
+    @Inject(method = "init(II)V", at = @At("TAIL"))
     private void randomisePanorama(CallbackInfo ci) {
         if (PanorandomClient.rerollMode == 1) {
             PanorandomClient.randomisePanorama();
